ba38671a2a23fa3877886b964d5551b173670104
Lexzt <fongzhizhong@gmail.com>
2018-01-03 01:30:22 +0800

    Re up to git

---
+++ FirebaseManager.js
@@ -0,0 +1,224 @@
+/**
+ * Firebase auth'
+ */
+var firebase = require("firebase");
+
+firebase.initializeApp({
+  serviceAccount: "SOC FOP Score bot-64daed28285e.json",
+  databaseURL: "https://soc-fop-score-bot.firebaseio.com/"
+});
+
+var db = firebase.database(),
+  ref = db.ref('Houses');
+
+var FirebaseManager = function () { };
+
+/**
+ * Function to create a new list
+ *
+ * @param  {[type]} listId   [description]
+ * @param  {[type]} listname [description]
+ * @return {[type]}          [description]
+ */
+FirebaseManager.prototype.createHouse = function (house_name, house_head, house_color) {
+  //Get current lis()
+  var s_list = "Houses",
+    s_listReference = house_color + ' House';
+
+  var listsRef = ref.child(s_listReference);
+
+  //Set datas to list
+  listsRef.set({
+    houseHead: house_head.first_name + " " + house_head.last_name,
+    houseColor: house_color,
+    houseName: house_name,
+    totalScore: 0
+  });
+};
+
+FirebaseManager.prototype.createOG = function (bot, listId, house_color, og_name, action) {
+  ref.once('value', function (snapshot) {
+    var listObj = snapshot.val();
+    console.log("test: " + listObj);
+    if (action === 'add') {
+      addOrientationGroupToHouse(bot, listId, listObj, house_color, og_name);
+    } else {
+      //removeItemOnArray(bot, listId, house_name, og_name, listObj);
+    }
+  });
+};
+
+FirebaseManager.prototype.addScoreGroups = function (bot, listId, house_color) {
+  var funcKeyboard = {
+    reply_markup: {
+      inline_keyboard:
+      [
+        [
+        ]
+      ],
+    }
+  }
+
+  ref.once("value", function (data) {
+    data.forEach(function (data) {
+      if (data.hasChild('houseColor')) {
+        if (data.child('houseColor').val() === house_color) {
+          data.child('orientationGroups').forEach(function (ogData) {
+            funcKeyboard.reply_markup.inline_keyboard[0].push(
+              {
+                text: ogData.val().ogName,
+                callback_data: JSON.stringify
+                  ({
+                    type: 'as,og',
+                    color: house_color,
+                    og: ogData.val().ogName,
+                    id: listId
+                  })
+              }
+            )
+          });
+          bot.sendMessage(listId, "Add Score, Choose OG", funcKeyboard)
+            .then(function () {
+              console.log('(as,og) ' + funcKeyboard);
+            }).catch(console.error);
+        }
+      }
+    });
+  });
+}
+
+FirebaseManager.prototype.updateScores = function (color, name, tscore) {
+  var house_scoreRef = ref.child(color + ' House/totalScore');
+  house_scoreRef.transaction(function (current_value) {
+    return (current_value || 0) + tscore;
+  });
+
+  var house_ogscoreRef = ref.child(color + ' House/orientationGroups');
+  house_ogscoreRef.orderByChild("ogName").equalTo(name).once("value", function (snapshot) {
+    snapshot.forEach(function (ogChild) {
+      console.log(ogChild.ref.toString());
+      ogChild.ref.child('Score').transaction(function (current_value) {
+        return (current_value || 0) + tscore;
+      });
+    })
+  });
+}
+
+FirebaseManager.prototype.listScores = function (bot, msgId) {
+  var str = "";
+  ref.once('value', function (snapshot) {
+    // console.log("snapshot: " + snapshot);
+    snapshot.forEach(function (snap) {
+      // console.log("snap: " +snap.val().totalScore);
+      str += jsUcfirst(snap.val().houseColor) + " house has " + snap.val().totalScore + " points!\n";
+      snap.child('orientationGroups').forEach(function (indivOg) {
+        // console.log(indivOg.val());
+        str += "OG " + indivOg.val().ogName + " scored " + indivOg.val().Score + " points!\n"
+      });
+    })
+    bot.sendMessage(msgId, str);
+    // console.log(str);
+  });
+}
+
+function jsUcfirst(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function objectExistsCallback(userId, exists, house_color, og_name) {
+  if (exists) {
+    // Add it into the list
+    var ogsRef = ref.child(house_color + ' House').child('orientationGroups');
+    ogsRef.push({ "ogName": og_name, "Score": 0 });
+  } else {
+    // Create it
+    var s_ogsReference = 'orientationGroups',
+      ogsRef = ref.child(house_color + ' House').child(s_ogsReference);
+
+    ogsRef.push({ "ogName": og_name, "Score": 0 });
+  }
+}
+
+function addOrientationGroupToHouse(bot, listId, participantsList, house_color, og_name) {
+  var list = "Houses";
+  var listReference = house_color + ' House';
+  var listsRef = ref.child(listReference);
+  var ogsRef = listsRef.child('orientationGroups');
+
+  ref.child(listReference).child('orientationGroups').once('value', function (snapshot) {
+    var exists = (snapshot.val() !== null);
+    objectExistsCallback('orientationGroups', exists, house_color, og_name);
+  });
+};
+
+function removeItemOnArray(bot, listId, participantsList, fullname) {
+  var list = listId.toString(),
+    listReference = 'list_' + list.replace(/-|\s/g, ''),
+    listsRef = ref.child(listReference),
+    arrParticipants = participantsList[listReference].participants || [],
+    index = arrParticipants.indexOf(fullname);
+
+  if (index > -1) {
+    arrParticipants.splice(index, 1);
+    bot.sendMessage(listId, 'Name was removed!');
+  } else {
+    bot.sendMessage(listId, 'This name not exists in the list');
+    return;
+  }
+
+  //Set datas to list
+  listsRef.update({
+    participants: arrParticipants
+  });
+};
+
+FirebaseManager.prototype.managerParticipants = function (bot, listId, firstName, lastName, action) {
+  ref.once('value', function (snapshot) {
+    var listObj = snapshot.val(),
+      fullname = firstName + ' ' + lastName;
+
+    if (action === 'add') {
+      addItemOnArray(bot, listId, listObj, fullname);
+    } else {
+      removeItemOnArray(bot, listId, listObj, fullname);
+    }
+
+  });
+};
+
+FirebaseManager.prototype.managerGuests = function (bot, listId, guestName, action) {
+  ref.once('value', function (snapshot) {
+    var listObj = snapshot.val(),
+      fullname = guestName;
+
+    if (action === 'add') {
+      addItemOnArray(bot, listId, listObj, fullname);
+    } else {
+      removeItemOnArray(bot, listId, listObj, fullname);
+    }
+
+  });
+};
+
+FirebaseManager.prototype.showList = function (bot, listId) {
+  ref.once("value", function (snapshot) {
+
+    var listObj = snapshot.val(),
+      list = listId.toString(),
+      listReference = 'list_' + list.replace(/-|\s/g, ''),
+      listsRef = ref.child(listReference),
+      participantsList = listObj[listReference].participants || [],
+      listName = listObj[listReference].listName,
+      output = listName + '\n';
+
+    //output
+    for (var i = 0; i < participantsList.length; i += 1) {
+      output += '' + (i + 1) + '. ' + participantsList[i] + '\n';
+    }
+
+    bot.sendMessage(listId, output);
+  });
+};
+
+
+module.exports = new FirebaseManager();
---
+++ bot.js
@@ -0,0 +1,146 @@
+var token = process.env.TOKEN;
+
+var Bot = require('node-telegram-bot-api');
+var bot;
+
+var FirebaseManager = require('./FirebaseManager.js');
+
+if (process.env.NODE_ENV === 'production') {
+  bot = new Bot(token);
+  bot.setWebHook('https://soc-fop-score-bot.herokuapp.com/' + bot.token);
+}
+else {
+  bot = new Bot(token, { polling: true });
+}
+
+/**
+ * matches /start
+ */
+bot.onText(/\/start/, function (msg, match) {
+  var fromId = msg.chat.id; // get the id, of who is sending the message
+  var message = "Welcome to your SOC FOP Bot\n";
+  message += "Create your house by using /create_house [house_name] [house_color] command.\n"
+  message += "Create your og by using /create_og [house_name] [og_name] command.\n"
+  message += "Add score by using /add_score command.\n"
+  bot.sendMessage(fromId, message);
+});
+
+bot.onText(/\/create_house (.+)/, function (msg, match) {
+  var house_name = match[1].split(' ')[0];
+  var house_color = match[1].split(' ')[1];
+  FirebaseManager.createHouse(house_name, msg.from, house_color);
+
+  var message = "House " + match[1] + " was created with color " + house_color;
+  bot.sendMessage(msg.chat.id, message);
+});
+
+//match /create [list name]
+bot.onText(/\/create_og (.+)/, function (msg, match) {
+  var house_color = match[1].split(' ')[0];
+  var og_name = match[1].split(' ')[1];
+
+  FirebaseManager.createOG(bot, msg.chat.id, house_color, og_name, 'add');
+  var message = "OG " + og_name + " was for house " + house_color;
+  bot.sendMessage(msg.chat.id, message);
+});
+
+bot.onText(/\/add_score/, function (msg, match) {
+  bot.sendMessage(msg.chat.id, "Add Score, Choose House", {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          { text: 'Red', callback_data: JSON.stringify({ type: "addScore, House", color: 'red', id: msg.chat.id }) },
+          { text: 'Blue', callback_data: JSON.stringify({ type: "addScore, House", color: 'blue', id: msg.chat.id }) },
+          { text: 'Green', callback_data: JSON.stringify({ type: "addScore, House", color: 'green', id: msg.chat.id }) },
+          { text: 'Yellow', callback_data: JSON.stringify({ type: "addScore, House", color: 'yellow', id: msg.chat.id }) },
+          { text: 'Gray', callback_data: JSON.stringify({ type: "addScore, House", color: 'gray', id: msg.chat.id }) }
+        ]
+      ],
+    },
+  }).then(function () {
+    // console.log('message sent');
+  }).catch(console.error);
+});
+
+bot.on("callback_query", function (msg) {
+  var obj = JSON.parse(msg.data);
+  if (obj.type === "addScore, House") {
+    // Start the looking for groups
+    FirebaseManager.addScoreGroups(bot, obj.id, obj.color);
+  } else if (obj.type === "as,og") {
+    bot.sendMessage(obj.id, "Add Score, Choose Points", {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            { text: '+10', callback_data: JSON.stringify({ type: "as,p", score: 10, color: obj.color, og: obj.og }) },
+            { text: '+20', callback_data: JSON.stringify({ type: "as,p", score: 20, color: obj.color, og: obj.og }) },
+            { text: '+30', callback_data: JSON.stringify({ type: "as,p", score: 30, color: obj.color, og: obj.og }) },
+            { text: '+40', callback_data: JSON.stringify({ type: "as,p", score: 40, color: obj.color, og: obj.og }) },
+            { text: '+50', callback_data: JSON.stringify({ type: "as,p", score: 50, color: obj.color, og: obj.og }) }
+          ]
+        ],
+      },
+    }).then(function () {
+      console.log('(as,p) ' + obj.color + ' - ' + obj.og);
+    }).catch(console.error);
+  } else if (obj.type === "as,p") {
+    bot.answerCallbackQuery(msg.id, 'Adding to house ' + obj.color + ", OG: " + obj.og + " with score: " + obj.score);
+
+    FirebaseManager.updateScores(obj.color, obj.og, obj.score);
+  }
+});
+
+bot.onText(/\/list_scores/, function (msg, match) {
+  FirebaseManager.listScores(bot, msg.chat.id);
+});
+
+/**
+ * matches /in
+ */
+bot.onText(/\/in/, function (msg, match) {
+  FirebaseManager.managerParticipants(bot, msg.chat.id, msg.from.first_name, msg.from.last_name, 'add');
+});
+
+/**
+ * matches /out
+ */
+bot.onText(/\/out/, function (msg, match) {
+  FirebaseManager.managerParticipants(bot, msg.chat.id, msg.from.first_name, msg.from.last_name, 'remove');
+});
+
+/**
+ * matches /add_guest
+ */
+bot.onText(/\/add_guest (.+)/, function (msg, match) {
+  FirebaseManager.managerGuests(bot, msg.chat.id, match[1], 'add');
+});
+
+/**
+  * matches /add_guest
+ */
+bot.onText(/\/remove_guest (.+)/, function (msg, match) {
+  FirebaseManager.managerGuests(bot, msg.chat.id, match[1], 'remove');
+});
+
+/**
+ * matches /showList
+ */
+bot.onText(/\/show/, function (msg, match) {
+  var list = FirebaseManager.showList(bot, msg.chat.id);
+});
+
+/**
+ * matches /help
+ */
+bot.onText(/\/help/, function (msg, match) {
+  var fromId = msg.chat.id; // get the id, of who is sending the message
+  var message = "To create a new list use: /create [your_list_name] \n";
+  message += "To add your name on the list use: /in \n";
+  message += "To remove your name from the list use: /out \n";
+  message += "To add guest's name on the list use: /add_guest [guest_name] \n";
+  message += "To remove guest's name from the list use: /remove_guest [guest_name] \n";
+  message += "To show the list use: /show \n";
+  bot.sendMessage(fromId, message);
+});
+
+module.exports = bot;
