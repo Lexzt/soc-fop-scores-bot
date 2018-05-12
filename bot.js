40ddd64bdff50eabb25605744b1bed260a809aa9
Lexzt <fongzhizhong@gmail.com>
2018-05-12 13:59:20 +0800

    Added comments for general understanding of current code.

--- bot.js
+++ bot.js
@@ -1,9 +1,13 @@
+// Token is taken from the file, .env. 
 var token = process.env.TOKEN;
 
+// Ensures the loading of the telegram api.
 var Bot = require('node-telegram-bot-api');
 var bot;
 
+// Ensures the loading of the firebase api
 var FirebaseManager = require('./FirebaseManager.js');
+
 
 if (process.env.NODE_ENV === 'production') {
   bot = new Bot(token);
@@ -26,6 +30,22 @@ bot.onText(/\/start/, function (msg, match) {
   bot.sendMessage(fromId, message);
 });
 
+/**
+ * matches /create_house (Params)
+ * Params are then split into 2.
+ * 1. Param 0 - House Name
+ * 2. Param 1 - House Color
+ * 
+ * Then creates a query to create a house in the database. 
+ * 
+ * To-do
+ * Error checking, 
+ * 0. More then 2 variable.
+ * 1. house name is not valid string format. 
+ * 2. house color is not valid string format.
+ * 
+ * 
+ */
 bot.onText(/\/create_house (.+)/, function (msg, match) {
   var house_name = match[1].split(' ')[0];
   var house_color = match[1].split(' ')[1];
@@ -35,7 +55,24 @@ bot.onText(/\/create_house (.+)/, function (msg, match) {
   bot.sendMessage(msg.chat.id, message);
 });
 
-//match /create [list name]
+/**
+ * Creates an OG in a House, based off the color.
+ * 
+ * matches /create_og (Params)
+ * 
+ * Params are then split into 2.
+ * 1. Param 0 - House Name
+ * 2. Param 1 - Og Name
+ * 
+ * Finds the specific house color, then creates the OG in the database
+ * 
+ * To-do
+ * Error handling, 
+ * 0. House color does not exist, send back deny message.
+ * 1. Og name exist, send back deny message.
+ * 2. Og name is not valid string. 
+ * 3. house color is not valid string format. 
+ */
 bot.onText(/\/create_og (.+)/, function (msg, match) {
   var house_color = match[1].split(' ')[0];
   var og_name = match[1].split(' ')[1];
@@ -45,6 +82,17 @@ bot.onText(/\/create_og (.+)/, function (msg, match) {
   bot.sendMessage(msg.chat.id, message);
 });
 
+/*
+ * Add scores based off the houses. 
+ * This function is more complex due to inline buttons. 
+ * 
+ * matches /add_score
+ * 
+ * No Params
+ * 
+ * It will send back a list of buttons to choose which house you want to add score to.
+ * Note, When you stringify the callback_data, type has to be less than 128 bytes. 
+ */
 bot.onText(/\/add_score/, function (msg, match) {
   bot.sendMessage(msg.chat.id, "Add Score, Choose House", {
     reply_markup: {
@@ -63,6 +111,22 @@ bot.onText(/\/add_score/, function (msg, match) {
   }).catch(console.error);
 });
 
+/*
+ * For callbacks
+ * 
+ * A callback will happen when an inline button is pressed. E.g. /add_score message.
+ * It will detech the add_score, and request for a number to be added, 
+ * 
+ * Say after a cheer, you have to add 10/20/30/40/50 based off the cheer. 
+ * This will make it easier to add score in. 
+ * 
+ * However, a slight problem is if you want to add custom amount of scores.
+ * 
+ * To-do
+ * 0. Custom amount of scores
+ * 1. A cancel button
+ * 2. 
+ */ 
 bot.on("callback_query", function (msg) {
   var obj = JSON.parse(msg.data);
   if (obj.type === "addScore, House") {
@@ -85,63 +149,15 @@ bot.on("callback_query", function (msg) {
       console.log('(as,p) ' + obj.color + ' - ' + obj.og);
     }).catch(console.error);
   } else if (obj.type === "as,p") {
-    bot.answerCallbackQuery(msg.id, 'Adding to house ' + obj.color + ", OG: " + obj.og + " with score: " + obj.score);
-
+    bot.answerCallbackQuery(msg.id, 'Adding to house ' + obj.color + 
+    ", OG: " + obj.og + 
+    " with score: " + obj.score);
     FirebaseManager.updateScores(obj.color, obj.og, obj.score);
   }
 });
 
 bot.onText(/\/list_scores/, function (msg, match) {
   FirebaseManager.listScores(bot, msg.chat.id);
-});
-
-/**
- * matches /in
- */
-bot.onText(/\/in/, function (msg, match) {
-  FirebaseManager.managerParticipants(bot, msg.chat.id, msg.from.first_name, msg.from.last_name, 'add');
-});
-
-/**
- * matches /out
- */
-bot.onText(/\/out/, function (msg, match) {
-  FirebaseManager.managerParticipants(bot, msg.chat.id, msg.from.first_name, msg.from.last_name, 'remove');
-});
-
-/**
- * matches /add_guest
- */
-bot.onText(/\/add_guest (.+)/, function (msg, match) {
-  FirebaseManager.managerGuests(bot, msg.chat.id, match[1], 'add');
-});
-
-/**
-  * matches /add_guest
- */
-bot.onText(/\/remove_guest (.+)/, function (msg, match) {
-  FirebaseManager.managerGuests(bot, msg.chat.id, match[1], 'remove');
-});
-
-/**
- * matches /showList
- */
-bot.onText(/\/show/, function (msg, match) {
-  var list = FirebaseManager.showList(bot, msg.chat.id);
-});
-
-/**
- * matches /help
- */
-bot.onText(/\/help/, function (msg, match) {
-  var fromId = msg.chat.id; // get the id, of who is sending the message
-  var message = "To create a new list use: /create [your_list_name] \n";
-  message += "To add your name on the list use: /in \n";
-  message += "To remove your name from the list use: /out \n";
-  message += "To add guest's name on the list use: /add_guest [guest_name] \n";
-  message += "To remove guest's name from the list use: /remove_guest [guest_name] \n";
-  message += "To show the list use: /show \n";
-  bot.sendMessage(fromId, message);
 });
 
 module.exports = bot;
