eab1992dc387ce0241cf8deebd5e9595dccd5f55
Lexzt <fongzhizhong@gmail.com>
2018-05-13 15:29:11 +0800

    Done basic approval

--- FirebaseManager.js
+++ FirebaseManager.js
@@ -233,7 +233,7 @@ FirebaseManager.prototype.checkIfApproved = function (userId) {
   console.log(userId);
   db.ref("Users").child(userId).once("value", function (snapshot) {
     console.log(snapshot.val());
-    return (snapshot.val() !== null);
+    return (snapshot.val() == null);
   });
 };
 
